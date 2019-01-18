b834814188895d7b6a32a07a0d27e85378ac979e
Haseeb Chaudhry <haseeb.chaudhry@hotmail.fr>
2019-01-18 01:10:22 +0100

    Init server project

--- Vagrantfile
+++ Vagrantfile
@@ -2,9 +2,11 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "optiDB-server"
   config.vm.hostname = "projetMaster-server"
+  config.vm.network "private_network", ip: "192.168.33.10"
+  config.vm.network "forwarded_port", guest: 3000, host: 3000, host_ip: "127.0.0.1"
   config.vm.provider "virtualbox" do |vb|
       vb.name = "projetMaster-server"
-      vb.memory = "1024"
+      vb.memory = "2048"
   end
    config.vm.provision "shell", path: 'scripts/install.sh'
 end
---
+++ src/main/java/optidb/server/OptiDbServerApplication.java
@@ -0,0 +1,28 @@
+package optidb.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class OptiDbServerApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(OptiDbServerApplication.class, args);
+	}
+
+	@RequestMapping(value="/test")
+	public String home()
+	{
+		System.out.println("La version est : " + SpringVersion.getVersion());
+		return "La version est : " + SpringVersion.getVersion();
+	}
+
+}
+
