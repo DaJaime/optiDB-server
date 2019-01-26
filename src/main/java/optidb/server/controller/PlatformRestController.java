58d3041dc247f3728315bc8d91808d9188a435cf
Unknown <37009779@parisnanterre.fr>
2019-01-26 17:30:29 +0100

    Add platform listing on API rest

--- src/main/java/optidb/server/controller/PlatformRestController.java
+++ src/main/java/optidb/server/controller/PlatformRestController.java
@@ -5,13 +5,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 @RestController
 public class PlatformRestController {
 
     @RequestMapping("/platform")
     public Platform platformVersion(@RequestParam(value="name", defaultValue="Inconu") String name) {
         name = name.toLowerCase();
-        return new Platform(name);
-
+        return new Platform(name, "0");
     }
+
+    @RequestMapping("/list")
+    public ArrayList <Platform> platformList() {
+        ArrayList<Platform> liste = new ArrayList<>();
+        String ligne = "";
+
+        try
+        {
+            // Création de la commande
+            Process process = Runtime.getRuntime().exec("docker images");
+            // On récup le résultat
+            BufferedReader reader = new BufferedReader( new InputStreamReader(process.getInputStream()));
+            // On passe la première ligne
+            ligne = reader.readLine();
+            // On lis les autres lignes, un crée une Platform par ligne que l'on ajoute à la liste des Platform
+            while ((ligne = reader.readLine()) != null) {
+                System.out.println(ligne);
+                String [] tab = ligne.split(" ");
+                liste.add(new Platform(tab[0], tab[15]));
+            }
+            process.waitFor();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+            return null;
+        }
+
+        return liste;
+    }
+
 }
--- src/main/java/optidb/server/model/Platform.java
+++ src/main/java/optidb/server/model/Platform.java
@@ -1,18 +1,12 @@
 package optidb.server.model;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Platform {
     private final String name;
     private final String currentVersion;
 
-    public Platform(String name) {
+    public Platform(String name, String version) {
         this.name = name;
-        this.currentVersion = this.version();
+        this.currentVersion = version;
     }
 
     public String getName() {
@@ -22,35 +16,5 @@ public class Platform {
     public String getCurrentVersion() {
         return currentVersion;
     }
-
-    public String version()
-    {
-        Logger logger = Logger.getLogger("logger");
-        try
-        {
-            // Création de la commande
-            Process process = Runtime.getRuntime().exec("mvn -v");
-            // On récup le résultat
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(process.getInputStream()));
-            String line = reader.readLine();
-            int exitVal = process.waitFor();
-            if (exitVal != 0)
-            {
-                logger.log(Level.INFO,"ERROR : Read version");
-            }
-            return line;
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-            return null;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            Thread.currentThread().interrupt();
-            return null;
-        }
-    }
-
 
 }
