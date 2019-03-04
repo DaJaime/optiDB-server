48afc2778f8d138b314b78d87ba28eaa58bd6898
Unknown <37009779@parisnanterre.fr>
2019-03-05 00:35:47 +0100

    Optimise Connect class

--- src/main/java/optidb/server/platformConnect/MariadbConnect.java
+++ src/main/java/optidb/server/platformConnect/MariadbConnect.java
@@ -1,57 +1,23 @@
 package optidb.server.platformConnect;
 
-import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.logging.Logger;
 
 public class MariadbConnect implements InterfaceConnect {
-    private static Logger myLog = Logger.getLogger("WarningLogging");
+    private Connect connect = new Connect();
 
     @Override
     public void dockerRun() {
-        try {
-            Process process = Runtime.getRuntime().exec("docker run -p 3306:3306 --name mariadb -e MYSQL_ROOT_PASSWORD=pass -e MYSQL_DATABASE=test -d mariadb:10.4");
-            process.waitFor();
-            Thread.sleep(20000);
-        } catch (IOException e) {
-            myLog.warning(e.toString());
-
-        } catch (InterruptedException e) {
-            myLog.warning(e.toString());
-            Thread.currentThread().interrupt();
-        }
-
+        connect.dockerRun("docker run -p 3306:3306 --name mariadb -e MYSQL_ROOT_PASSWORD=pass -e MYSQL_DATABASE=test -d mariadb:10.4");
     }
 
     @Override
     public Connection connect() {
-        Connection cx = null;
-        try {
-            cx = DriverManager.getConnection("jdbc:mariadb://172.17.0.2:3306/test","root","pass");
-        }
-        catch (SQLException e) { // accès à la base refusé
-            myLog.warning(e.toString());
-        }
+        Connection cx = connect.connect("jdbc:mariadb://172.17.0.2:3306/test");
         return cx;
     }
 
     @Override
     public void dockerClose(Connection cx) {
-        try {
-            if (cx != null) cx.close();
-            Process processClose = Runtime.getRuntime().exec("docker stop mariadb");
-            processClose.waitFor();
-            Thread.sleep(4000);
-            Process processRm = Runtime.getRuntime().exec("docker rm mariadb");
-            processRm.waitFor();
-            Thread.sleep(4000);
-        } catch (IOException | SQLException e) {
-            myLog.warning(e.toString());
-        } catch (InterruptedException e) {
-            myLog.warning(e.toString());
-            Thread.currentThread().interrupt();
-        }
+        connect.dockerClose("mariadb", cx);
     }
 }
--- src/main/java/optidb/server/platformConnect/PostgresConnect.java
+++ src/main/java/optidb/server/platformConnect/PostgresConnect.java
@@ -1,55 +1,23 @@
 package optidb.server.platformConnect;
 
-import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.logging.Logger;
 
 public class PostgresConnect implements InterfaceConnect {
-    private static Logger myLog = Logger.getLogger("WarningLogging");
+    private Connect connect = new Connect();
+
     @Override
     public void dockerRun() {
-        try {
-            Process process = Runtime.getRuntime().exec("docker run -p 5432:5432 --name postgres -e POSTGRES_USER=root -e POSTGRES_PASSWORD=pass -e POSTGRES_DB=test -d postgres:11.2");
-            process.waitFor();
-            Thread.sleep(20000);
-        } catch (IOException e) {
-            myLog.warning(e.toString());
-
-        } catch (InterruptedException e) {
-            myLog.warning(e.toString());
-            Thread.currentThread().interrupt();
-        }
+        connect.dockerRun("docker run -p 5432:5432 --name postgres -e POSTGRES_USER=root -e POSTGRES_PASSWORD=pass -e POSTGRES_DB=test -d postgres:11.2");
     }
 
     @Override
     public Connection connect() {
-        Connection cx = null;
-        try {
-            cx = DriverManager.getConnection("jdbc:postgresql://172.17.0.2:5432/test","root","pass");
-        }
-        catch (SQLException e) { // accès à la base refusé
-            myLog.warning(e.toString());
-        }
+        Connection cx = connect.connect("jdbc:postgresql://172.17.0.2:5432/test");
         return cx;
     }
 
     @Override
     public void dockerClose(Connection cx) {
-        try {
-            if (cx != null) cx.close();
-            Process processClose = Runtime.getRuntime().exec("docker stop postgres");
-            processClose.waitFor();
-            Thread.sleep(4000);
-            Process processRm = Runtime.getRuntime().exec("docker rm postgres");
-            processRm.waitFor();
-            Thread.sleep(4000);
-        } catch (IOException|SQLException e) {
-            myLog.warning(e.toString());
-        } catch (InterruptedException e) {
-            myLog.warning(e.toString());
-            Thread.currentThread().interrupt();
-        }
+        connect.dockerClose("postgres", cx);
     }
 }
