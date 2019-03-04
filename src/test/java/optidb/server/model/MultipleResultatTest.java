b0f5f8f9b62e71b1541cea82f3657be5efb664b9
Unknown <37009779@parisnanterre.fr>
2019-03-04 20:29:09 +0100

    Add MultipleResultatTest

---
+++ src/test/java/optidb/server/model/MultipleResultatTest.java
@@ -0,0 +1,31 @@
+package optidb.server.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class MultipleResultatTest {
+    MultipleResultat res;
+
+    @Before
+    public void setUp() throws Exception {
+        ArrayList listeInsert = new ArrayList();
+        Resultat resuUn = new Resultat("Test",1, 1, 1, listeInsert, 1,
+                1, 1, 1,1, 1);
+        ArrayList<Resultat> array = new ArrayList<>();
+        array.add(resuUn);
+        res = new MultipleResultat(array);
+    }
+
+    @Test
+    public void getListResu() throws Exception {
+        ArrayList listeInsert = new ArrayList();
+        Resultat newResu = new Resultat("Test",1, 1, 1, listeInsert,
+                1, 1, 1, 1,1, 1);
+        assertEquals(newResu, res.getListResu().get(0));
+    }
+
+}
